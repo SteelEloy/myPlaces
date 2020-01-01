25c8b9fcd875f296ce08f618186ee2fb77948fd9
SteelEloy <eloy_2106@yahoo.com>
2020-01-01 02:52:42 +0200

    initial

---
+++ myPlaces/MainViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MainViewController.swift
+//  myPlaces
+//
+//  Created by Саня Eloy on 31.12.2019.
+//  Copyright © 2019 Саня Eloy. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    let restaurantName = [
+    "Nonna", "Santorini Cafe", "Pasta & Pizza"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return restaurantName.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = restaurantName[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantName[indexPath.row])
+
+        return cell
+    }
+    
+
+    /*
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
