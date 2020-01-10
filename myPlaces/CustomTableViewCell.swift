ef501411622c647ab43d6fa24aa8f65520788d42
SteelEloy <eloy_2106@yahoo.com>
2020-01-11 01:16:54 +0200

    Custom Table View Cell

---
+++ myPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  myPlaces
+//
+//  Created by Саня Eloy on 11.01.2020.
+//  Copyright © 2020 Саня Eloy. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+}
